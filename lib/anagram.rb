182122a9dba66eea035cb6650a95953e1fd50ad9
Naftali Kulik <nkulik1994@gmail.com>
2022-06-21 16:19:02 -0400

    commit tree f0d82b3607bdbff2afe92b9b6880195a21a77ab6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match word_arr
+        word_arr.filter { |anagram| anagram.chars.sort == word.chars.sort }
+    end
+end
